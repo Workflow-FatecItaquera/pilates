aafc27bbd7069727ae52f4ff78868c8cab02a890
trinitydevs <trinitynascimento@gmail.com>
2025-05-17 12:43:50 -0300

    FEATURE: ajustes cadastrar instrutor

---
+++ src/main/java/com/pilates/workflow/controller/AlunoRoute.java
@@ -0,0 +1,13 @@
+package com.pilates.workflow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AlunoRoute {
+	@GetMapping("/cadastrarAluno")
+	public String cadastrarInstrutor() {
+		return "cadastrarInstrutor";
+	}
+	
+}
