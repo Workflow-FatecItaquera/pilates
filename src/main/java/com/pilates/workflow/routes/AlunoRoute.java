113143431e7fe8dfb29c80db61fe29e49ebc80cf
Joia <gustavo.sc.joia@gmail.com>
2025-05-23 17:56:15 -0300

    FEAT: Visual-traits no formulário de aluno

--- src/main/java/com/pilates/workflow/routes/AlunoRoute.java
+++ src/main/java/com/pilates/workflow/routes/AlunoRoute.java
@@ -1,12 +1,23 @@
 package com.pilates.workflow.routes;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.pilates.workflow.service.AssinaturaService;
+import com.pilates.workflow.model.Aluno;
 
 @Controller
 public class AlunoRoute {
+
+	@Autowired
+	private AssinaturaService assinaturaService;
+
 	@GetMapping("/cadastrarAluno")
-	public String cadastrarAluno() {
+	public String cadastrarAluno(Model model) {
+		model.addAttribute("aluno", new Aluno());
+		model.addAttribute("assinaturas", assinaturaService.getAll());
 		return "cadastrarAluno";
 	}
 	
