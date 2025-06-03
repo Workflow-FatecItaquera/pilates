928e368d33c0b0aa0f3f38a851de1aa94a2c94e4
Joia <gustavo.sc.joia@gmail.com>
2025-06-03 04:42:11 -0300

    FEAT: Login de instrutor

--- src/main/java/com/pilates/workflow/routes/SenhaRoute.java
+++ src/main/java/com/pilates/workflow/routes/SenhaRoute.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.pilates.workflow.model.Aluno;
+import com.pilates.workflow.model.Instrutor;
 import com.pilates.workflow.service.AlunoService;
+import com.pilates.workflow.service.InstrutorService;
 import com.pilates.workflow.service.TokenService;
 
 @Controller
@@ -19,6 +21,8 @@ public class SenhaRoute {
     private TokenService tokenService;
     @Autowired
     private AlunoService alunoService;
+    @Autowired
+    private InstrutorService instrutorService;
 
     @GetMapping("/alunoSenha")
     public String alunoSenha(@RequestParam("token") String token) {
@@ -29,6 +33,16 @@ public class SenhaRoute {
         }
     }
 
+    @GetMapping("/instrutorSenha")
+    public String getMethodName(@RequestParam("token") String token) {
+        if(!tokenService.isTokenValid(token)){
+            return "redirect:/";
+        } else {
+            return "instrutorSenha";
+        }
+    }
+    
+
     @PostMapping("/aluno/senha")
     public String criarSenha(@RequestParam("token") String token, @RequestParam("senha") String senha, RedirectAttributes redirectAttributes) {
         if(tokenService.isTokenValid(token)){
@@ -36,6 +50,22 @@ public class SenhaRoute {
             Aluno aluno = alunoService.getByEmail(email);
             aluno.setSenha(senha);
             alunoService.register(aluno);
+
+            redirectAttributes.addFlashAttribute("senhaCriada",true);
+            return "redirect:/login";
+        } else {
+            redirectAttributes.addFlashAttribute("falhou",token);
+            return "redirect:/";
+        }
+    }
+
+    @PostMapping("/instrutor/senha")
+    public String criarSenhaInstrutor(@RequestParam("token") String token, @RequestParam("senha") String senha, RedirectAttributes redirectAttributes) {
+        if(tokenService.isTokenValid(token)){
+            String email = tokenService.extractEmail(token);
+            Instrutor instrutor = instrutorService.getByEmail(email);
+            instrutor.setSenha(senha);
+            instrutorService.register(instrutor);
 
             redirectAttributes.addFlashAttribute("senhaCriada",true);
             return "redirect:/login";
--- src/main/resources/static/js/senhaHandler.js
+++ src/main/resources/static/js/senhaHandler.js
@@ -1,14 +1,14 @@
-let senha = document.querySelectorAll('input')[0];
-let confirme = document.querySelectorAll('input')[1];
+let senha = document.querySelectorAll('input')[1];
+let confirme = document.querySelectorAll('input')[2];
 let form = document.querySelector('form');
 
-let erro_senha = document.querySelectorAll('.mensagem-erro')[1];
+let erro_senha = document.querySelectorAll('.mensagem-erro')[0];
 erro_senha.textContent = '';
-let erro_confirme = document.querySelectorAll('.mensagem-erro')[2];
+let erro_confirme = document.querySelectorAll('.mensagem-erro')[1];
 erro_confirme.textContent = '';
 
 senha.addEventListener('input',function(){  
-    if(senha.value.length<8){
+    if(senha.value.length<8 && senha.value.length!=0){
         erro_senha.textContent = 'mínimo de 8 caractéres';
     } else {
         erro_senha.textContent = '';
@@ -17,7 +17,6 @@ senha.addEventListener('input',function(){
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
-
     if(senha.value!=confirme.value) {
         erro_confirme.textContent = 'as senhas não coincidem';
     } else {
