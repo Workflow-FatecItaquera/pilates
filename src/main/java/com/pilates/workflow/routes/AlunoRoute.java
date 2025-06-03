1fc71b662a81a03ae2c615500ae085573beb852b
Joia <gustavo.sc.joia@gmail.com>
2025-06-02 22:13:28 -0300

    teste

--- src/main/java/com/pilates/workflow/routes/AlunoRoute.java
+++ src/main/java/com/pilates/workflow/routes/AlunoRoute.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.pilates.workflow.service.AssinaturaService;
+import com.pilates.workflow.service.EmailService;
 import com.pilates.workflow.service.AlunoService;
 import com.pilates.workflow.model.Aluno;
 
@@ -17,6 +18,7 @@ public class AlunoRoute {
 	@Autowired
 	private AssinaturaService assinaturaService;
 	private AlunoService alunoService;
+	private EmailService emailService;
 
 	@GetMapping("/cadastrarAluno")
 	public String cadastrarAluno(Model model) {
--- src/main/java/com/pilates/workflow/routes/HomeRoute.java
+++ src/main/java/com/pilates/workflow/routes/HomeRoute.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 //Informa ao Spring que se trata de uma classe @Controller com requisições HTTP.
 public class HomeRoute {
 	@GetMapping
