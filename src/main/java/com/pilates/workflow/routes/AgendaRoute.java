93079be4537af92f39ea9aee40a3868d68c8bc8c
Joia <gustavo.sc.joia@gmail.com>
2025-06-23 05:43:58 -0300

    checkpoint

--- src/main/java/com/pilates/workflow/routes/AgendaRoute.java
+++ src/main/java/com/pilates/workflow/routes/AgendaRoute.java
@@ -1,14 +1,23 @@
 package com.pilates.workflow.routes;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.pilates.workflow.details.InstrutorDetails;
+import com.pilates.workflow.model.EventoAgendado;
+import com.pilates.workflow.model.Instrutor;
+import com.pilates.workflow.service.InstrutorService;
+
 //import com.pilates.workflow.model.Aluno;
 
 @Controller
 public class AgendaRoute {
+
+	@Autowired
+	InstrutorService instrutorService;
 
 	@GetMapping("/cadastrarAula")
 	public String cadastrarAula(Model model, Authentication auth) {
@@ -17,7 +26,11 @@ public class AgendaRoute {
 		if(!isInstrutor){
 			return "redirect:/dashboard";
 		}
-
+		InstrutorDetails details = (InstrutorDetails) auth.getPrincipal();
+		Instrutor instrutor = details.getInstrutor();
+		model.addAttribute("evento", new EventoAgendado());
+		model.addAttribute("instrutores", instrutorService.getAll());
+		model.addAttribute("instrutor", instrutor);
 		return "cadastrarAula";
 	}
 }
